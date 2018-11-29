9484680a36f2dd8de3e6ed36b3834ed6c2b48a01
vang1ong7ang <vang1ong7ang@outlook.com>
2018-11-29 11:17:44 +0800

    simple go

---
+++ demo/simple/js/simple.js
@@ -0,0 +1,59 @@
+function simple() {
+    let graph = {
+        vertices: [
+            {
+                id: 'TT',
+                value: null,
+            },
+            {
+                id: 'TF',
+                value: null,
+            },
+            {
+                id: 'FT',
+                value: null,
+            },
+            {
+                id: 'FF',
+                value: null,
+            },
+        ],
+        edges: [
+            {
+                x: 'TT',
+                y: 'TF',
+            },
+            {
+                x: 'TF',
+                y: 'FF',
+            },
+            {
+                x: 'FF',
+                y: 'FT',
+            },
+            {
+                x: 'FT',
+                y: 'TT',
+            },
+        ],
+    }
+    let game = vgo(graph)
+    let turn = true;
+    return {
+        play: function (id) {
+            if (id !== null) {
+                game.go_next({
+                    id: id,
+                    value: turn,
+                })
+            }
+            turn = !turn
+        },
+        status:function(){
+            return game.get_vertices()
+        },
+        turn: function () {
+            return turn;
+        }
+    }
+}
